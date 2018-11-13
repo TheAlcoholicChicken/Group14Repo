eb37bf7da49e1952d2b41d35239c42879ede5325
Darian Gratton <Dariangratton@gmail.com>
2018-11-13 13:12:25 -0800

    Added function for api call

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -8,8 +8,9 @@ var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/nodetest1')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const user = require('./routes/user.route')
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(function(req,res,next) {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/user/', user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
