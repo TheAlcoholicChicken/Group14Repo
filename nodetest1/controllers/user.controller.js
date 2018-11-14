24f4b7bcc391ed78715e9ae8e04b9a262220a1c2
Darian Gratton <Dariangratton@gmail.com>
2018-11-14 13:20:26 -0800

    Added main functionally for the API

--- nodetest1/controllers/user.controller.js
+++ nodetest1/controllers/user.controller.js
@@ -1,4 +1,26 @@
 exports.get_badge_message = function (req, res) {
-    // Sean and Jaisreet put code here for the api
-    res.send('Greetings from the Test controller!');
+
+    let db = req.db;
+    let userId = req.body.userId;
+
+    db.collection("Player").find({}, {}, function(e, docs) {
+        let userElement = null;
+        
+        docs.forEach(element => {
+            if (element.user_id === userId) {
+                userElement = element;
+            }
+        });
+
+        if (userElement === null) {
+            res.status(401).send({ error: 'Invalid UserID' });
+        } else {
+            res.status(200);
+            res.json({
+                user_id: userElement.user_id,
+                badge_text: "Top " + userElement.data.best_ranking + " in Ranking"
+            });
+            res.end();
+        }
+    });
 }
