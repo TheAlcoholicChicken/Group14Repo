e74fb4a052a314d4dc206856a26cd3c6d77e5d5b
Darian Gratton <Dariangratton@gmail.com>
2018-11-13 17:39:45 -0800

    Added token implementation

--- nodetest1/routes/user.route.js
+++ nodetest1/routes/user.route.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const user_controller = require('../controllers/user.controller');
+const config = require('../config.js');
 
-router.get('/get_badge_message', user_controller.get_badge_message);
+function authToken(req, res, next) {
+    let token = req.body.token;
+
+    if (!token) res.status(401).send({ error: "Token Missing" });
+
+    if (token === config.token) {
+        next();
+    } else {
+        res.status(401).send({ error: 'Invalid Token' });
+    }
+}
+
+router.post('/get_badge_message', authToken, user_controller.get_badge_message);
 module.exports = router;
